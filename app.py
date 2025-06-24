31aed68972c2cd74d5aa9057dbf7fe3af1caf179
Lan Anh <dhlananh2309@gmail.com>
2025-06-24 14:38:40 +0700

    fix: Update langchain import for vectorstores

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from PyPDF2 import PdfReader
 
